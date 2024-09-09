97f9a7c5d332984fd45be6668f49ad5f9a8f93e1
AdamBaugher <adambaugher42@gmail.com>
2024-09-09 07:38:18 -0500

    [feat] Initialize Project using TailwindCSS.

--- warehouse-app/app/page.tsx
+++ warehouse-app/app/page.tsx
@@ -1,7 +1,21 @@
-export const metadata = {
-  title: "App Router",
-};
+import Image from "next/image";
+import Link from 'next/link';
 
-export default function Page() {
-  return <h1>App Router</h1>;
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
+          <h1 className="text-2xl font-bold text-gray-800 text-center mb-6">
+            Warehouse Dashboard
+          </h1>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <p>
+          Cozey Assessment
+        </p>
+      </footer>
+    </div>
+  );
 }
